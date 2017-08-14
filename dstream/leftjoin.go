ace12a803ee564b733494c6ede68a9745a4517f8
brookluers <BrookLuers@gmail.com>
2017-08-14 16:01:05 -0400

    convert uint64 to float64 in linapply, remove debugging

--- dstream/leftjoin.go
+++ dstream/leftjoin.go
@@ -2,6 +2,7 @@ package dstream
 
 import (
 	"math"
+	"fmt"
 )
 
 // idcols: names of id column in each Dstream to join
@@ -26,8 +27,10 @@ func LeftJoin(ds1, ds2 Dstream, idcols, addcols []string) Dstream {
 	var l0 int
 	var rlength, ldiff int
 	var rdat, temp []float64
+	totrec := 0
 	for join.Next() {
 		l0 = len(da[0].GetPos(vm0[idcols[0]]).([]uint64)) // length of added columns for this chunk
+		totrec+= l0
 		if join.Status[1] {
 			for j := 0; j < len(addcols); j++ {
 				rdat = da[1].GetPos(vm1[addcols[j]]).([]float64)
@@ -50,17 +53,20 @@ func LeftJoin(ds1, ds2 Dstream, idcols, addcols []string) Dstream {
 				}
 
 			}
-		} else { // no match on the id column
+		} else { // no match on the id column	
 			temp = make([]float64, l0)
 			for k := 0; k < l0; k++ {
 				temp[k] = math.NaN()
+				fmt.Printf("writing nan\n")
 			}
 			for j := 0; j < len(addcols); j++ {
 				newdat[j] = append(newdat[j], temp...)
 			}
 		}
 	}
+
 	for j, newcol := range newdat {
+	       //fmt.Printf("\n---leftjoin length of column %v: %d\n", addcols[j], len(newcol))
 		ret = Addcol(ret, newcol, addcols[j])
 	}
 	return ret
